29177e340f127e71cab53f93895209c5d3391498
Takasur Azeem <takasurazeem@mtbc.com>
2021-07-26 21:05:43 +0500

    scrolling dealt

--- TableViewWithFAB/ViewController.swift
+++ TableViewWithFAB/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var fab: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        hideFAB()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            showFAB()
+        } else {
+            hideFAB()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        showFAB()
+    }
+    
+    func showFAB() {
+        fab.isHidden = false
+    }
+    
+    func hideFAB() {
+        fab.isHidden = true
+    }
+}
